0b47b4968010d1b11eb9122f4d319227201face3
Salvador Murillo <chavitation@gmail.com>
2021-02-06 23:44:20 -0800

    added modal and last touch ups

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 
+function openModal(){
+    $("#modalbody").css('display','block');
+}
 
+window.onload = function(){
+    setTimeout(openModal,1000);
+}
+
+$(document).on('click',"#modalClose",function(){
+    $("#modalbody").css('display','none');
+})
 
 $("#submit").on("click",function(e){
 
@@ -15,6 +25,8 @@ $("#submit").on("click",function(e){
         method: "GET",
         dataType:'jsonp'
     }).then(function(response){
+        $("audio").css("visibility","visible");
+        $(".songDeets").css("display","block");
         var id = response.data[0].artist.id;
         console.log(response);
         console.log(response.data[0].artist.picture_xl)
@@ -30,15 +42,21 @@ $("#submit").on("click",function(e){
             method:"GET",
             dataType:'jsonp'
         }).then(function(trackList){
+            $(".listenOn").css("visibility","visible");
+            $("#other").css("visibility","visible");
             console.log(trackList);
-            var randomNum = Math.floor(Math.random() * 10);
                 $(".discover1").css("background-image","url(" + trackList.data[3].album.cover_medium + ")")
                 $(".discover2").css("background-image","url(" + trackList.data[4].album.cover_medium + ")")
                 $(".discover3").css("background-image","url(" + trackList.data[5].album.cover_medium + ")")
                 $("#clip2").attr("src",trackList.data[3].preview)
                 $("#clip3").attr("src",trackList.data[4].preview)
                 $("#clip4").attr("src",trackList.data[5].preview)
-    
+                $("#title2").html(trackList.data[3].title)
+                $("#artist2").html(trackList.data[3].artist.name)
+                $("#title3").html(trackList.data[4].title)
+                $("#artist3").html(trackList.data[4].artist.name)
+                $("#title4").html(trackList.data[5].title)
+                $("#artist4").html(trackList.data[5].artist.name)
         })
 
     })
